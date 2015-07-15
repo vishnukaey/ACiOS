dc03a5c5675a9fd875a6f600edb6c1a5fbc8cd08
Govind_Gopinath <govind@qburst.com>
2015-07-15 11:19:17 +0530

    Added ViewControllers Registration

---
+++ LegacyConnect/ViewControllers/LCChooseCausesVC/LCChooseCausesVC.h
@@ -0,0 +1,13 @@
+//
+//  LCChooseCausesVC.h
+//  LegacyConnect
+//
+//  Created by qbuser on 7/15/15.
+//  Copyright (c) 2015 Gist. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LCChooseCausesVC : UIViewController
+
+@end
---
+++ LegacyConnect/ViewControllers/LCChooseCausesVC/LCChooseCausesVC.m
@@ -0,0 +1,37 @@
+//
+//  LCChooseCausesVC.m
+//  LegacyConnect
+//
+//  Created by qbuser on 7/15/15.
+//  Copyright (c) 2015 Gist. All rights reserved.
+//
+
+#import "LCChooseCausesVC.h"
+
+@interface LCChooseCausesVC ()
+
+@end
+
+@implementation LCChooseCausesVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
