9977d2b3aed46eefe1790bd032b6438581a5ea34
Govind_Gopinath <govind@qburst.com>
2015-07-17 17:13:32 +0530

    changes pagecontroller

---
+++ LegacyConnect/CustomViews/LCChooseCausesCollectionViewCell/LCChooseCausesCollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  LCChooseCausesCollectionViewCell.m
+//  LegacyConnect
+//
+//  Created by qbuser on 7/17/15.
+//  Copyright (c) 2015 Gist. All rights reserved.
+//
+
+#import "LCChooseCausesCollectionViewCell.h"
+
+@implementation LCChooseCausesCollectionViewCell
+
+@end
