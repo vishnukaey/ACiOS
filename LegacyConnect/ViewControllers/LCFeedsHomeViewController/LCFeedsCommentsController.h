dad17afe3de51af60db0f46d6034a6992412bfae
jijo <jijojohnpy@gmail.com>
2015-08-07 17:27:47 +0530

    Auto layout and custom navigation bar in feed comments page.

--- LegacyConnect/ViewControllers/LCFeedsHomeViewController/LCFeedsCommentsController.h
+++ LegacyConnect/ViewControllers/LCFeedsHomeViewController/LCFeedsCommentsController.h
@@ -13,8 +13,9 @@
 @interface LCFeedsCommentsController : UIViewController<UITableViewDataSource, UITableViewDelegate, feedCellDelegate, UITextFieldDelegate>
 {
   NSMutableArray *H_cellsViewArray;
-  UITableView *H_mainTable;
+  IBOutlet UITableView *H_mainTable;
   UITextField *H_commmentTextField, *H_dup;//h_dup is for pushing the keyboard as it wont push for commentfield as it is the input accessory view
 }
+- (IBAction)backAction;
 
 @end
