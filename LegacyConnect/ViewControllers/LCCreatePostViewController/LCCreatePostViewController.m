298a5afa0055bcb90d10105e73fe6f793d91ea75
Govind_Office <Office@Gos-MacBook-Pro.local>
2015-08-12 10:54:51 +0530

    adding file for creating a post

---
+++ LegacyConnect/ViewControllers/LCCreatePostViewController/LCCreatePostViewController.m
@@ -0,0 +1,69 @@
+//
+//  LCCreatePostViewController.m
+//  LegacyConnect
+//
+//  Created by Govind_Office on 11/08/15.
+//  Copyright (c) 2015 Gist. All rights reserved.
+//
+
+#import "LCCreatePostViewController.h"
+#import "LCContactsListVC.h"
+
+@interface LCCreatePostViewController ()
+{
+  LCContactsListVC *contactListVC;
+}
+@end
+
+@implementation LCCreatePostViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+  [_postTextView becomeFirstResponder];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+
+- (IBAction)closeButtonClicked:(id)sender
+{
+  [self.delegate dismissView];
+  [self.view removeFromSuperview];
+}
+
+- (void)textViewDidChange:(UITextView *)textView
+{
+  if (textView.text.length>0)
+  {
+    _placeHolderText.hidden = YES;
+  }
+  else
+  {
+    _placeHolderText.hidden = NO;
+  }
+}
+
+
+- (IBAction)addFriendsToPostButtonClicked:(id)sender
+{
+  UIStoryboard*  sb = [UIStoryboard storyboardWithName:@"SignUp" bundle:nil];
+  contactListVC = [sb instantiateViewControllerWithIdentifier:@"ContactList"];
+  [self presentViewController:contactListVC animated:YES completion:nil];
+
+}
+
+@end
