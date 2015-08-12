96b3f8a34eb7ddb5c85b74ebef3a51ff78bdd568
Govind_Office <Office@Gos-MacBook-Pro.local>
2015-08-12 10:55:14 +0530

    adding file to list friends to tag

---
+++ LegacyConnect/ViewControllers/LCListFriendsToTagViewController/LCListFriendsToTagViewController.h
@@ -0,0 +1,18 @@
+//
+//  LCListFriendsToTagViewController.h
+//  LegacyConnect
+//
+//  Created by Govind_Office on 11/08/15.
+//  Copyright (c) 2015 Gist. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LCListFriendsToTagViewController : UIViewController<UITableViewDataSource,UITableViewDelegate>
+
+
+@property(nonatomic,retain) IBOutlet UIBarButtonItem *cancelButton;
+@property(nonatomic,retain) IBOutlet UIBarButtonItem *doneButton;
+@property(nonatomic,retain) IBOutlet UINavigationBar *navigationBar;
+@property(nonatomic,retain) IBOutlet UITableView *tableView;
+@end
---
+++ LegacyConnect/ViewControllers/LCListFriendsToTagViewController/LCListFriendsToTagViewController.m
@@ -0,0 +1,37 @@
+//
+//  LCListFriendsToTagViewController.m
+//  LegacyConnect
+//
+//  Created by Govind_Office on 11/08/15.
+//  Copyright (c) 2015 Gist. All rights reserved.
+//
+
+#import "LCListFriendsToTagViewController.h"
+
+@interface LCListFriendsToTagViewController ()
+
+@end
+
+@implementation LCListFriendsToTagViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
