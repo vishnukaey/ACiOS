6e1f06037e92be80cea14a5b4b39ec2e53ba0441
sarath <sarath@qburst.com>
2015-09-18 11:01:33 +0530

    enabling and disabling submit button in passsword reset flow based on text field text length

--- LegacyConnect/ViewControllers/LCUpdatePasswordViewController/LCUpdatePasswordViewController.m
+++ LegacyConnect/ViewControllers/LCUpdatePasswordViewController/LCUpdatePasswordViewController.m
@@ -12,6 +12,7 @@
 static NSString * kResetPasswordTitle = @"RESET PASSWORD";
 
 @interface LCUpdatePasswordViewController ()
+@property (weak, nonatomic) IBOutlet UIButton *updateButton;
 
 @end
 
@@ -28,6 +29,47 @@ static NSString * kResetPasswordTitle = @"RESET PASSWORD";
   [backButton addTarget:self action:@selector(backButtonPressed:) forControlEvents:UIControlEventTouchUpInside];
   UIBarButtonItem * backButtonItem = [[UIBarButtonItem alloc] initWithCustomView:backButton];
   [self.navigationItem setLeftBarButtonItem:backButtonItem];
+  [self changeUpdateButtonState];
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+  [super viewWillAppear:animated];
+  [self addTextFieldTextDidChangeNotifiaction];
+}
+
+- (void)viewWillDisappear:(BOOL)animated
+{
+  [self removeTextFieldTextDidChangeNotifiaction];
+  [super viewWillDisappear:animated];
+}
+
+
+#pragma mark - private method implementation
+- (void)addTextFieldTextDidChangeNotifiaction
+{
+  [[NSNotificationCenter defaultCenter] addObserver:self
+                                           selector:@selector(changeUpdateButtonState)
+                                               name:UITextFieldTextDidChangeNotification
+                                             object:nil];
+}
+
+- (void)removeTextFieldTextDidChangeNotifiaction
+{
+  [[NSNotificationCenter defaultCenter] removeObserver:self
+                                                  name:UITextFieldTextDidChangeNotification
+                                                object:nil];
+}
+
+- (void)changeUpdateButtonState
+{
+  if (self.passwordTextField.text.length > 0 && self.confirmPasswordTextField.text.length > 0) {
+    [self.updateButton setEnabled:true];
+  }
+  else
+  {
+    [self.updateButton setEnabled:false];
+  }
 }
 
 - (void)backButtonPressed:(id)sender
