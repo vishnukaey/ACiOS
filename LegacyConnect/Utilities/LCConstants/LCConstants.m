39e5c343a13c3b1ea53dbaaaff3343f8d905d483
sarath <sarath@qburst.com>
2015-09-30 11:50:50 +0530

    navigation drawer changes

--- LegacyConnect/Utilities/LCConstants/LCConstants.m
+++ LegacyConnect/Utilities/LCConstants/LCConstants.m
@@ -126,5 +126,6 @@ NSString *const kLCUrlScheme = @"legacyconnect";
 NSString *const kResetPasswordTokenKey = @"password_reset_token";
 NSString *const kResetPasswordNotificationName = @"password_reset_notification";
 
+NSString *const kUserImageChangeNotification = @"user_image_change_notification";
 
 @end
