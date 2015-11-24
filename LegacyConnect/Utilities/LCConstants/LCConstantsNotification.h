04e85a69de2f8d6698d33da9239ee2e3776a9945
jijo <jijojohnpy@gmail.com>
2015-11-24 15:49:13 +0530

    NSNotification constants added

--- LegacyConnect/Utilities/LCConstants/LCConstantsNotification.h
+++ LegacyConnect/Utilities/LCConstants/LCConstantsNotification.h
@@ -14,6 +14,29 @@ extern NSString *const kTwitterCallbackNotification;
 
 extern NSString *const kUserDataUpdatedNotification;
 
+extern NSString *const kUpdateProfileNFK;
+extern NSString *const kCreateNewPostNFK;
+extern NSString *const kUpdatePostNFK;
+extern NSString *const kDeletePostNFK;
+extern NSString *const kLikedPostNFK;
+extern NSString *const kUnlikedPostNFK;
+extern NSString *const kCommentPostNFK;
+extern NSString *const kRemoveMileStoneNFK;
+extern NSString *const kFollowInterestNFK;
+extern NSString *const kUnfollowInterestNFK;
+extern NSString *const kSupportCauseNFK;
+extern NSString *const kUnsupportCauseNFK;
+extern NSString *const kSendFriendRequestNFK;
+extern NSString *const kCancelFriendRequestNFK;
+extern NSString *const kRemoveFriendNFK;
+extern NSString *const kAcceptFriendRequestNFK;
+extern NSString *const kCreateEventNFK;
+extern NSString *const kCommentEventNFK;
+extern NSString *const kDeleteEventNFK;
+extern NSString *const kUpdateEventNFK;
+extern NSString *const kFollowEventNFK;
+extern NSString *const kUnfollowEventNFK;
+
 extern NSString *const kUserProfileUpdateNotification;
 extern NSString *const kUserProfileFrinendsUpdateNotification;
 extern NSString *const kUserProfileImpactsUpdateNotification;
--- LegacyConnect/Utilities/LCConstants/LCConstantsNotification.m
+++ LegacyConnect/Utilities/LCConstants/LCConstantsNotification.m
@@ -14,6 +14,28 @@ NSString *const kTwitterCallbackNotification = @"twitter_callback_notification";
 
 NSString *const kUserDataUpdatedNotification = @"user_data_updated_notification";
 
+NSString *const kUpdateProfileNFK = @"profile_updated";
+NSString *const kCreateNewPostNFK = @"new_post_created";
+NSString *const kUpdatePostNFK = @"post_updated";
+NSString *const kDeletePostNFK = @"post_deleted";
+NSString *const kLikedPostNFK = @"post_liked";
+NSString *const kUnlikedPostNFK = @"post_unliked";
+NSString *const kCommentPostNFK = @"post_commented";
+NSString *const kRemoveMileStoneNFK = @"milestone_removed";
+NSString *const kFollowInterestNFK = @"interest_followed";
+NSString *const kUnfollowInterestNFK = @"interest_unfollowed";
+NSString *const kSupportCauseNFK = @"cause_supported";
+NSString *const kUnsupportCauseNFK = @"cause_unsupported";
+NSString *const kSendFriendRequestNFK = @"friend_request_sent";
+NSString *const kCancelFriendRequestNFK = @"friend_request_canceled";
+NSString *const kRemoveFriendNFK = @"friend_removed";
+NSString *const kAcceptFriendRequestNFK = @"friend_request_accepted";
+NSString *const kCreateEventNFK = @"event_created";
+NSString *const kCommentEventNFK = @"event_commented";
+NSString *const kDeleteEventNFK = @"event_deleted";
+NSString *const kUpdateEventNFK = @"event_updated";
+NSString *const kFollowEventNFK = @"event_followed";
+NSString *const kUnfollowEventNFK = @"event_unfollowed";
 
 NSString *const kUserProfileUpdateNotification = @"userProfileUpdated";
 NSString *const kUserProfileFrinendsUpdateNotification = @"userProfileFriendsUpdated";
