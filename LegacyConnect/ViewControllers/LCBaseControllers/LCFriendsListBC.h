7e275cf4ce7a3033dd8fdf09d3c7bbbf68f62709
jijo <jijojohnpy@gmail.com>
2015-11-26 15:53:51 +0530

    Users/friends list data refreshing

--- LegacyConnect/ViewControllers/LCBaseControllers/LCFriendsListBC.h
+++ LegacyConnect/ViewControllers/LCBaseControllers/LCFriendsListBC.h
@@ -10,4 +10,6 @@
 
 @interface LCFriendsListBC : LCUsersListBC
 
+@property (nonatomic, strong) NSString * userId;
+
 @end
--- LegacyConnect/ViewControllers/LCBaseControllers/LCUsersListBC.m
+++ LegacyConnect/ViewControllers/LCBaseControllers/LCUsersListBC.m
@@ -20,13 +20,64 @@
 @implementation LCUsersListBC
 
 - (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view.
+  [super viewDidLoad];
+  
+  [[NSNotificationCenter defaultCenter] addObserver:self
+                                           selector:@selector(friendStatusUpdatedNotificationReceived:)
+                                               name:kSendFriendRequestNFK
+                                             object:nil];
+  [[NSNotificationCenter defaultCenter] addObserver:self
+                                           selector:@selector(friendStatusUpdatedNotificationReceived:)
+                                               name:kCancelFriendRequestNFK
+                                             object:nil];
+  [[NSNotificationCenter defaultCenter] addObserver:self
+                                           selector:@selector(friendStatusUpdatedNotificationReceived:)
+                                               name:kRemoveFriendNFK
+                                             object:nil];
+  [[NSNotificationCenter defaultCenter] addObserver:self
+                                           selector:@selector(friendStatusUpdatedNotificationReceived:)
+                                               name:kAcceptFriendRequestNFK
+                                             object:nil];
+  
 }
 
 - (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+  [super didReceiveMemoryWarning];
+}
+
+- (void)dealloc
+{
+  [[NSNotificationCenter defaultCenter] removeObserver:self];
+}
+
+- (void)friendStatusUpdatedNotificationReceived :(NSNotification *)notification
+{
+  LCFriend *newFriend = notification.userInfo[@"friend"];
+  for (int i = 0; i<self.results.count ; i++) {
+    if ([self.results[i] isKindOfClass:[LCUserDetail class]]) {
+      LCUserDetail *friend = self.results[i];
+      if ([friend.userID isEqualToString:newFriend.friendId])
+      {
+        friend.isFriend = newFriend.isFriend;
+        break;
+      }
+    }
+  }
+  [self refreshViews];
+}
+
+- (void)refreshViews
+{
+    CGPoint offset = self.tableView.contentOffset;
+    [self.tableView reloadData];
+    [self.tableView layoutIfNeeded]; // Force layout so things are updated before resetting the contentOffset.
+    [self.tableView setContentOffset:offset];
+}
+
+- (void) viewWillAppear:(BOOL)animated
+{
+  [super viewWillAppear:animated];
+  [self refreshViews];
 }
 
 /*
