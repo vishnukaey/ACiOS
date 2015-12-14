1ee77c16b40a759e4a0571a0087a9ef7717d9569
Akhil K C <akhilchalil@qburst.com>
2015-12-14 18:11:46 +0530

    final selection ui

--- LegacyConnect/ViewControllers/LCEmptyViewController/LCEmptyViewController.m
+++ LegacyConnect/ViewControllers/LCEmptyViewController/LCEmptyViewController.m
@@ -318,13 +318,13 @@ static NSString *kTitle = @"MY FEED";
   }
   else if (index == 2)//settings
   {
-//    UIStoryboard*  sb = [UIStoryboard storyboardWithName:kSettingsStoryBoardIdentifier bundle:nil];
-//    LCSettingsViewController *vc = [sb instantiateViewControllerWithIdentifier:kSettingsStoryBoardID];
-//    [navigationRoot setViewControllers:[NSArray arrayWithObject:vc]];
-    
-    UIStoryboard*  sb = [UIStoryboard storyboardWithName:kSignupStoryBoardIdentifier bundle:nil];
-    LCOnboardCausesVC *vc = [sb instantiateViewControllerWithIdentifier:@"LCOnboardCausesVC"];
+    UIStoryboard*  sb = [UIStoryboard storyboardWithName:kSettingsStoryBoardIdentifier bundle:nil];
+    LCSettingsViewController *vc = [sb instantiateViewControllerWithIdentifier:kSettingsStoryBoardID];
     [navigationRoot setViewControllers:[NSArray arrayWithObject:vc]];
+    
+//    UIStoryboard*  sb = [UIStoryboard storyboardWithName:kSignupStoryBoardIdentifier bundle:nil];
+//    LCOnboardCausesVC *vc = [sb instantiateViewControllerWithIdentifier:@"LCOnboardCausesVC"];
+//    [navigationRoot setViewControllers:[NSArray arrayWithObject:vc]];
     
     
 //    UIStoryboard*  sb = [UIStoryboard storyboardWithName:kSignupStoryBoardIdentifier bundle:nil];
