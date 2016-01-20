1c30dd9c8015e0c47d48beaffe94449e49a6ceff
jijo <jijojohnpy@gmail.com>
2016-01-20 12:27:08 +0530

    Get contact method splitter

--- LegacyConnect/Utilities/LCUtilityManager/LCUtilityManager.m
+++ LegacyConnect/Utilities/LCUtilityManager/LCUtilityManager.m
@@ -259,8 +259,102 @@
 //}
 
 #pragma mark- Get Contacts
-+ (NSArray *)getPhoneContacts {
++ (NSString *)getFirstnameOfContactForPerson :(ABRecordRef)person
+{
+  CFStringRef firstName = (CFStringRef)ABRecordCopyValue(person,kABPersonFirstNameProperty);
+  NSString *firstName_ = [(__bridge NSString*)firstName copy];
+  if (firstName != NULL) {
+    CFRelease(firstName);
+  }
+  if (!firstName_) {
+    firstName_ = @"";
+  }
+  return firstName_;
+}
+
++ (NSString *)getLastnameOfContactForPerson :(ABRecordRef)person
+{
+  CFStringRef lastName = (CFStringRef)ABRecordCopyValue(person,kABPersonLastNameProperty);
+  NSString *lastName_ = [(__bridge NSString*)lastName copy];
   
+  if (lastName != NULL) {
+    CFRelease(lastName);
+  }
+  if (!lastName_) {
+    lastName_ = @"";
+  }
+  return lastName_;
+}
+
++ (UIImage *)getImageOfContactForPerson :(ABRecordRef)person
+{
+  CFDataRef imgData = ABPersonCopyImageData(person);
+  NSData *imageData = (__bridge NSData *)imgData;
+  UIImage *dpImage = [UIImage imageWithData:imageData];
+  if (imgData != NULL) {
+    CFRelease(imgData);
+  }
+  if (!dpImage) {
+    dpImage = [UIImage imageNamed:@"userProfilePic"];
+  }
+  return dpImage;
+}
+
++ (NSArray *)getPhoneNumbersOfContactForPerson :(ABRecordRef)person
+{
+  NSMutableArray *phoneNumbers = [[NSMutableArray alloc] init];
+  ABMultiValueRef multiPhones = ABRecordCopyValue(person, kABPersonPhoneProperty);
+  
+  for(CFIndex i=0; i<ABMultiValueGetCount(multiPhones); i++) {
+    @autoreleasepool {
+      CFStringRef phoneNumberRef = ABMultiValueCopyValueAtIndex(multiPhones, i);
+      NSString *phoneNumber = CFBridgingRelease(phoneNumberRef);
+      if (phoneNumber != nil)[phoneNumbers addObject:phoneNumber];
+      //NSLog(@"All numbers %@", phoneNumbers);
+    }
+  }
+  
+  if (multiPhones != NULL) {
+    CFRelease(multiPhones);
+  }
+  return phoneNumbers;
+}
+
++ (NSArray *)getEmailsOfContactForPerson :(ABRecordRef)person
+{
+  NSMutableArray *contactEmails = [NSMutableArray new];
+  ABMultiValueRef multiEmails = ABRecordCopyValue(person, kABPersonEmailProperty);
+
+  for (CFIndex i=0; i<ABMultiValueGetCount(multiEmails); i++) {
+    @autoreleasepool {
+      CFStringRef contactEmailRef = ABMultiValueCopyValueAtIndex(multiEmails, i);
+      NSString *contactEmail = CFBridgingRelease(contactEmailRef);
+      if (contactEmail != nil)[contactEmails addObject:contactEmail];
+      // NSLog(@"All emails are:%@", contactEmails);
+    }
+  }
+
+  if (multiEmails != NULL) {
+    CFRelease(multiEmails);
+  }
+  return contactEmails;
+}
+
++ (NSString *)getLocationOfContactForPerson :(ABRecordRef)person
+{
+  NSString *address = @"";
+  ABMultiValueRef addressRef = ABRecordCopyValue(person, kABPersonAddressProperty);
+  if (ABMultiValueGetCount(addressRef) > 0) {
+    NSDictionary *addressDict = (__bridge NSDictionary *)ABMultiValueCopyValueAtIndex(addressRef, 0);
+    
+    address = [NSString stringWithFormat:@"%@",[addressDict objectForKey:(NSString *)kABPersonAddressCityKey]];
+    
+  }
+  CFRelease(addressRef);
+  return address;
+}
+
++ (NSArray *)getPhoneContacts {
     CFErrorRef *error = nil;
     ABAddressBookRef addressBook = ABAddressBookCreateWithOptions(NULL, error);
     ABRecordRef source = ABAddressBookCopyDefaultSource(addressBook);
@@ -268,136 +362,42 @@
     //CFIndex nPeople = ABAddressBookGetPersonCount(addressBook);
     CFIndex nPeople = CFArrayGetCount(allPeople); // bugfix who synced contacts with facebook
     NSMutableArray* items = [NSMutableArray arrayWithCapacity:nPeople];
-    
-    if (!allPeople || !nPeople) {
-        NSLog(@"people nil");
-    }
-    
-    
+//    if (!allPeople || !nPeople) {
+//        NSLog(@"people nil");
+//    }
     for (int i = 0; i < nPeople; i++) {
-        
         @autoreleasepool {
-            
             //data model
             LCContact *contact_ = [[LCContact alloc] init];
-            
             ABRecordRef person = CFArrayGetValueAtIndex(allPeople, i);
-            
             //get First Name
-            CFStringRef firstName = (CFStringRef)ABRecordCopyValue(person,kABPersonFirstNameProperty);
-            NSString *firstName_ = [(__bridge NSString*)firstName copy];
-            
-            if (firstName != NULL) {
-                CFRelease(firstName);
-            }
-            if (!firstName_) {
-                firstName_ = @"";
-            }
-            
-            
+            NSString *firstName_ = [LCUtilityManager getFirstnameOfContactForPerson:person];
             //get Last Name
-            CFStringRef lastName = (CFStringRef)ABRecordCopyValue(person,kABPersonLastNameProperty);
-            NSString *lastName_ = [(__bridge NSString*)lastName copy];
-            
-            if (lastName != NULL) {
-                CFRelease(lastName);
-            }
-            
-            
-            
-            
-            if (!lastName_) {
-                lastName_ = @"";
-            }
-            
-            
-            
+            NSString *lastName_ = [LCUtilityManager getLastnameOfContactForPerson:person];
             //            contacts.contactId = ABRecordGetRecordID(person);
             //append first name and last name
             contact_.P_name = [NSString stringWithFormat:@"%@ %@", firstName_, lastName_];
-            
             if ([contact_.P_name isEqualToString:@" "]) {
                 continue;
             }
-            
             // get contacts picture, if pic doesn't exists, show standart one
-            CFDataRef imgData = ABPersonCopyImageData(person);
-            NSData *imageData = (__bridge NSData *)imgData;
-            contact_.P_image = [UIImage imageWithData:imageData];
-            
-            if (imgData != NULL) {
-                CFRelease(imgData);
-            }
-            
-            if (!contact_.P_image) {
-                contact_.P_image = [UIImage imageNamed:@"userProfilePic"];
-            }
-            
-            
+            contact_.P_image = [LCUtilityManager getImageOfContactForPerson:person];
+          
             //get Phone Numbers
-            NSMutableArray *phoneNumbers = [[NSMutableArray alloc] init];
-            ABMultiValueRef multiPhones = ABRecordCopyValue(person, kABPersonPhoneProperty);
-            
-            for(CFIndex i=0; i<ABMultiValueGetCount(multiPhones); i++) {
-                @autoreleasepool {
-                    CFStringRef phoneNumberRef = ABMultiValueCopyValueAtIndex(multiPhones, i);
-                    NSString *phoneNumber = CFBridgingRelease(phoneNumberRef);
-                    if (phoneNumber != nil)[phoneNumbers addObject:phoneNumber];
-                    //NSLog(@"All numbers %@", phoneNumbers);
-                }
-            }
-            
-            if (multiPhones != NULL) {
-                CFRelease(multiPhones);
-            }
-            
-            contact_.P_numbers = phoneNumbers;
+            contact_.P_numbers = [LCUtilityManager getPhoneNumbersOfContactForPerson:person];
             
 //            if (contact_.P_numbers.count == 0) {
 //                continue;
 //            }
           
             //get Contact email
-            NSMutableArray *contactEmails = [NSMutableArray new];
-            ABMultiValueRef multiEmails = ABRecordCopyValue(person, kABPersonEmailProperty);
-            
-            for (CFIndex i=0; i<ABMultiValueGetCount(multiEmails); i++) {
-                @autoreleasepool {
-                    CFStringRef contactEmailRef = ABMultiValueCopyValueAtIndex(multiEmails, i);
-                    NSString *contactEmail = CFBridgingRelease(contactEmailRef);
-                    if (contactEmail != nil)[contactEmails addObject:contactEmail];
-                    // NSLog(@"All emails are:%@", contactEmails);
-                }
-            }
-            
-            if (multiPhones != NULL) {
-                CFRelease(multiEmails);
-            }
-            
-            contact_.P_emails = contactEmails;
-            
+            contact_.P_emails = [LCUtilityManager getEmailsOfContactForPerson:person];
             if (contact_.P_emails.count == 0) {
                 continue;
             }
-          
           //get location
-          contact_.P_address = @"";
-          ABMultiValueRef addressRef = ABRecordCopyValue(person, kABPersonAddressProperty);
-          if (ABMultiValueGetCount(addressRef) > 0) {
-            NSDictionary *addressDict = (__bridge NSDictionary *)ABMultiValueCopyValueAtIndex(addressRef, 0);
-            
-            contact_.P_address = [NSString stringWithFormat:@"%@",[addressDict objectForKey:(NSString *)kABPersonAddressCityKey]];
-           
-          }
-          CFRelease(addressRef);
+            contact_.P_address = [LCUtilityManager getLocationOfContactForPerson:person];
             [items addObject:contact_];
-            
-#ifdef DEBUG
-            NSLog(@"Person is: %@", contact_.P_name);
-            NSLog(@"Phones are: %@", contact_.P_numbers);
-            NSLog(@"Email is:%@", contact_.P_emails);
-#endif
-            
         }
     } //autoreleasepool
     CFRelease(allPeople);
