5165af29100949c25c0c4aa847ee8530b2794e6b
sarath <sarath@qburst.com>
2016-01-29 13:36:56 +0530

    added report post details screen

---
+++ LegacyConnect/ViewControllers/LCReportPostViewController/LCReportPostViewController.h
@@ -0,0 +1,13 @@
+//
+//  LCReportPostViewController.h
+//  LegacyConnect
+//
+//  Created by qbuser on 29/01/16.
+//  Copyright © 2016 Gist. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LCReportPostViewController : UIViewController
+
+@end
