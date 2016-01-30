8a0820b453318549e7c48fbd08f490191238a6b9
Akhil K C <akhilchalil@qburst.com>
2016-01-30 14:10:44 +0530

    block user action sheet option added

--- LegacyConnect/ViewControllers/LCInterestsVC/LCSingleCauseVC.m
+++ LegacyConnect/ViewControllers/LCInterestsVC/LCSingleCauseVC.m
@@ -14,7 +14,7 @@
 #import "LCProfileViewVC.h"
 #import "LCCauseSupportersVC.h"
 #import "LCSingleInterestVC.h"
-#import "LCReportPostViewController.h"
+#import "LCReportHelper.h"
 
 static NSString* const kGradientDefaultColor = @"282828";
 
@@ -265,21 +265,7 @@ static NSString* const kGradientDefaultColor = @"282828";
 
 - (void)reportFeed:(LCFeed*)feed
 {
-  UIAlertController *actionSheet = [UIAlertController alertControllerWithTitle:nil message:nil preferredStyle:UIAlertControllerStyleActionSheet];
-  actionSheet.view.tintColor = [UIColor blackColor];
-  
-  UIAlertAction *reportPost = [UIAlertAction actionWithTitle:NSLocalizedString(@"report_post", nil) style:UIAlertActionStyleDestructive handler:^(UIAlertAction *action) {
-    UIStoryboard*  mainSB = [UIStoryboard storyboardWithName:kMainStoryBoardIdentifier
-                                                      bundle:nil];
-    LCReportPostViewController *report = [mainSB instantiateViewControllerWithIdentifier:@"LCReportPostViewController"];
-    report.postToReport = feed;
-    [self presentViewController:report animated:YES completion:nil];
-  }];
-  [actionSheet addAction:reportPost];
-  
-  UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:NSLocalizedString(@"cancel", nil) style:UIAlertActionStyleCancel handler:nil];
-  [actionSheet addAction:cancelAction];
-  [self presentViewController:actionSheet animated:YES completion:nil];
+  [LCReportHelper showPostReportActionSheetFromView:self withPost:feed];
 }
 
 
