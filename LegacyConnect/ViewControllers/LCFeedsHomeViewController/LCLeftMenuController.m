34ac19d2fe8a575604f4fe111fe047f74c00a695
vishnuk <vishnukaey@gmail.com>
2016-03-01 20:06:34 +0530

    version update

--- LegacyConnect/ViewControllers/LCFeedsHomeViewController/LCLeftMenuController.m
+++ LegacyConnect/ViewControllers/LCFeedsHomeViewController/LCLeftMenuController.m
@@ -54,7 +54,7 @@ static NSString * kMenuCellIdentifier = @"LCMenuItemCell";
   NSDictionary* infoDict = [[NSBundle mainBundle] infoDictionary];
   NSString* version = infoDict[@"CFBundleShortVersionString"];
   [_versionLabel setText:[NSString stringWithFormat:@"ThatHelps! %@",version]];
-//  [_versionLabel setHidden:YES];
+  [_versionLabel setHidden:YES];
 }
 
 - (void)refreshUserInfo
